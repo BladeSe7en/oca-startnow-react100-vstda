e0690bac8d6c775ea5b8cf7a5488de7082d18039
BrianC <seahorse8789@gmail.com>
2018-09-17 14:39:29 -0700

    update

--- server/server.js
+++ server/server.js
@@ -7,16 +7,5 @@ app.use(morgan('dev'));
 app.use(express.static('dist'));
 app.use(express.static('public'));
 
-function convertCurrency () {
-    $.ajax({
-        type: "GET",
-            url: "https://api.exchangeratesapi.io/latest?base=USD",
-    
-            success: function(data){
-                console.log(data);
-                var echange_rate = '';
-            }
-        });
-    }
-    convertCurrency();
+
 module.exports = app;
