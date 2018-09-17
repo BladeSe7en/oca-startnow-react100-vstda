5ddc8a08fec007de450555dea1c1a325b1e6c16c
BrianC <seahorse8789@gmail.com>
2018-09-17 14:14:04 -0700

    Ready for deployment

--- server/server.js
+++ server/server.js
@@ -7,4 +7,16 @@ app.use(morgan('dev'));
 app.use(express.static('dist'));
 app.use(express.static('public'));
 
+function convertCurrency () {
+    $.ajax({
+        type: "GET",
+            url: "https://api.exchangeratesapi.io/latest?base=USD",
+    
+            success: function(data){
+                console.log(data);
+                var echange_rate = '';
+            }
+        });
+    }
+    convertCurrency();
 module.exports = app;
